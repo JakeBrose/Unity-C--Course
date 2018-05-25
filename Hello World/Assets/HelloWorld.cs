0d2becb20f3169f70ecf5c0983daa126df1af8f7
JakeBrose <mordecaistudios@gmail.com>
2018-05-24 19:20:04 -0500

    initial commit, contains hello world file

---
+++ Hello World/Assets/HelloWorld.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	void Start () {
+        print("Hello Jake!");
+	}
+	
+	void Update () {
+		
+	}
+}
