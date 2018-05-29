21cc63e6725db419e8e8baf5fca8ffd4e8193747
JakeBrose <mordecaistudios@gmail.com>
2018-05-29 16:03:17 -0500

    lesson 1 intro pt.2

--- beginnerC#/program.cs
+++ beginnerC#/program.cs
@@ -2,6 +2,9 @@ class program
 {
     static void Main()
     {
-        
+        // Prompt user for minutes excerised 
+        // Add minutes exercised to running total
+        // Display total minutes exercised to view
+        // Repeat until user quits
     }
 }
