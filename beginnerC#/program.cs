6dc6ab363765000092d31edba7f81c372d3c803f
JakeBrose <mordecaistudios@gmail.com>
2018-05-30 16:30:18 -0500

    beginner ex.4, naming conventions

--- beginnerC#/program.cs
+++ beginnerC#/program.cs
@@ -1,13 +1,30 @@
-class program 
-{
-    static void Main()
-    {
-        // Prompt user for minutes excerised 
-        System.Console.Write("Enter how many minutes you exercised: ");
+////////////////////
+// general notes //
+//////////////////
 
-        string entry = System.Console.ReadLine();
-        // Add minutes exercised to running total
-        // Display total minutes exercised to view
-        // Repeat until user quits
+// mcs compiles code into exe
+// mono runs the .exe file
+// namespace.class.method = order of names in a method call
+
+/////////////////
+
+///////////////////
+// Fitness Frog //
+/////////////////
+
+namespace Tutorial.FitnessFrog
+{
+    class program
+    {
+        static void Main()
+        {
+            // Prompt user for minutes excerised 
+            System.Console.Write("Enter how many minutes you exercised: ");
+
+            string entry = System.Console.ReadLine();
+            // Add minutes exercised to running total
+            // Display total minutes exercised to view
+            // Repeat until user quits
+        }
     }
 }
