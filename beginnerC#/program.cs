b28e1b9ae6d44b8bcaad2264e5ec7a236629da2f
JakeBrose <mordecaistudios@gmail.com>
2018-05-30 11:09:43 -0500

    beginner ex.3, System.Console.ReadLine

--- beginnerC#/program.cs
+++ beginnerC#/program.cs
@@ -4,6 +4,8 @@ class program
     {
         // Prompt user for minutes excerised 
         System.Console.Write("Enter how many minutes you exercised: ");
+
+        string entry = System.Console.ReadLine();
         // Add minutes exercised to running total
         // Display total minutes exercised to view
         // Repeat until user quits
