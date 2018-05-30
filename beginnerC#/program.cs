e1b24ec6e93317bf7a12b3b0f99d2c0322cbb4ad
JakeBrose <mordecaistudios@gmail.com>
2018-05-30 10:52:37 -0500

    beginner ex.2, mcs and mono

--- beginnerC#/program.cs
+++ beginnerC#/program.cs
@@ -3,6 +3,7 @@ class program
     static void Main()
     {
         // Prompt user for minutes excerised 
+        System.Console.Write("Enter how many minutes you exercised: ");
         // Add minutes exercised to running total
         // Display total minutes exercised to view
         // Repeat until user quits
