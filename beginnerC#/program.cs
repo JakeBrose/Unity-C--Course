bbd9516ca660494ac7abdb063cf26b1ba0bcb328
JakeBrose <mordecaistudios@gmail.com>
2018-05-31 11:31:14 -0500

    beginner ex.7, while loops and if/else

--- beginnerC#/program.cs
+++ beginnerC#/program.cs
@@ -22,18 +22,28 @@ namespace Tutorial.FitnessFrog
         static void Main()
         {
             int runningTotal = 0;
-            // Prompt user for minutes excerised 
-            Console.Write("Enter how many minutes you exercised: ");
-            string entry = Console.ReadLine();
-
-            // Add minutes exercised to running total
-            int minutes = int.Parse(entry);
-            runningTotal = runningTotal + minutes;                 
-            // Display total minutes exercised to view
-            // "+" is used to concatenate strings
-            Console.WriteLine("You have exercised " + runningTotal + " minutes.");
-
-            // Repeat until user quits
+            bool keepGoing = true;
+            while (keepGoing) 
+            {
+                // Prompt user for minutes excerised 
+                Console.Write("Enter how many minutes you exercised, or type quit: ");
+                string entry = Console.ReadLine();
+                // Repeat until user quits 
+                if (entry == "quit")
+                {
+                    keepGoing = false;
+                    Console.WriteLine("Goodbye.");
+                }
+                else
+                {
+                    // Add minutes exercised to running total
+                    int minutes = int.Parse(entry);
+                    runningTotal = runningTotal + minutes;
+                    // Display total minutes exercised to view
+                    // "+" is used to concatenate strings
+                    Console.WriteLine("You have exercised " + runningTotal + " minutes.");
+                }
+            }
         }
     }
 }
