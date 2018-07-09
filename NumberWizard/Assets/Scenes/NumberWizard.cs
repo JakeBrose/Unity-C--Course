e24e076772ecf2b2e15968ed2571f2db4cd57e3b
JakeBrose <mordecaistudios@gmail.com>
2018-07-09 12:18:14 -0500

    challenge 1: add statements using debug

---
+++ NumberWizard/Assets/Scenes/NumberWizard.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Welcome to Number Wizard");
+        Debug.Log("Pick a number");
+        Debug.Log("Highest: 1000");
+        Debug.Log("Lowest: 1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
