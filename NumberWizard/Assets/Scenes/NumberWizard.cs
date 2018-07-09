a72838f78a71f1bcd6b761eee501cd3b64ca3463
JakeBrose <mordecaistudios@gmail.com>
2018-07-09 12:21:23 -0500

    updated messages

--- NumberWizard/Assets/Scenes/NumberWizard.cs
+++ NumberWizard/Assets/Scenes/NumberWizard.cs
@@ -7,9 +7,9 @@ public class NumberWizard : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         Debug.Log("Welcome to Number Wizard");
-        Debug.Log("Pick a number");
-        Debug.Log("Highest: 1000");
-        Debug.Log("Lowest: 1");
+        Debug.Log("Pick a number--don't tell me what it is!");
+        Debug.Log("The highest number you may choose is 1000.");
+        Debug.Log("The lowest number you may choose is 1.");
 	}
 	
 	// Update is called once per frame
